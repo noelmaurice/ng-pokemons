9586ba6f1bc07132d88589565201c4ee868d9b60
Noël MAURICE <noelmaurice4@gmail.com>
2021-02-05 16:05:08 +0100

    commit

--- src/app/pokemons/list-pokemon.component.ts
+++ src/app/pokemons/list-pokemon.component.ts
@@ -7,52 +7,52 @@ import { Router } from '@angular/router';
 import { PokemonsService } from "./pokemons.service";
 
 @Component({
-  selector: 'app-list-pokemons',
-  templateUrl: './list-pokemon.component.html', // path vers fichier HTML
-  // providers: [PokemonsService]
+	selector: 'app-list-pokemons',
+	templateUrl: './list-pokemon.component.html', // path vers fichier HTML
+	// providers: [PokemonsService]
 })
 
 export class ListPokemonComponent implements OnInit {
 
-  private pokemons: Pokemon[] = null;
-  private title: string = 'Pokémons';
+	private pokemons: Pokemon[] = null;
+	private title: string = 'Pokémons';
 
-  /**
-   * la variable private de type PokemonsService permet l'injection d'une instance du service dans le composant
-   * l'instance du service est unique pour tout le composant mais aussi pour toute l'application
-   */ 
-  constructor(private router: Router, private pokemonsService: PokemonsService) {
-    /**
-     * A NE PAS FAIRE :
-     * 1. Si le constructeur de PokemonsService change, il faut modifier tous les composants qui utilsent ce service
-     * 2. A chaque new, une nouvelle instance de PokemonsService est créée
-     * 3. L'utilisateur ne doit pas connaitre comment PokemonsService est construit
-     */
-    // let pokemons = new PokemonsService();
-  }
+	/**
+	 * la variable private de type PokemonsService permet l'injection d'une instance du service dans le composant
+	 * l'instance du service est unique pour tout le composant mais aussi pour toute l'application
+	 */
+	constructor(private router: Router, private pokemonsService: PokemonsService) {
+		/**
+		 * A NE PAS FAIRE :
+		 * 1. Si le constructeur de PokemonsService change, il faut modifier tous les composants qui utilsent ce service
+		 * 2. A chaque new, une nouvelle instance de PokemonsService est créée
+		 * 3. L'utilisateur ne doit pas connaitre comment PokemonsService est construit
+		 */
+		// let pokemons = new PokemonsService();
+	}
 
-  ngOnInit() {
-    // this.pokemons = POKEMONS; // référence déclarée dans les imports
+	ngOnInit() {
+		// this.pokemons = POKEMONS; // référence déclarée dans les imports
 
-    // utilisation du service pour récupérer tous les pokémons
-    // this.pokemons = this.pokemonsService.getPokemons();
+		// utilisation du service pour récupérer tous les pokémons
+		// this.pokemons = this.pokemonsService.getPokemons();
 
-    // utilisation d'un Observable à la place du service initial
-    this.getPokemons();
-  }
+		// utilisation d'un Observable à la place du service initial
+		this.getPokemons();
+	}
 
-  // utilisation d'un Observable à la place du service initial
-  getPokemons(): void {
+	// utilisation d'un Observable à la place du service initial
+	getPokemons(): void {
 		this.pokemonsService.getPokemons()
 			.subscribe(pokemons => this.pokemons = pokemons);
 	}
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log('Vous avez selectionné ' + pokemon.name);
+	selectPokemon(pokemon: Pokemon) {
+		console.log('Vous avez selectionné ' + pokemon.name);
 
-    // URL + paramètre
-    const link = ['/pokemon', pokemon.id];
-    this.router.navigate(link);
-  }
+		// URL + paramètre
+		const link = ['/pokemon', pokemon.id];
+		this.router.navigate(link);
+	}
 
 }
